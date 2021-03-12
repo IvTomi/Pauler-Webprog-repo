ad9599ed99d0a52afb531762dba24f32bce925d9
SAdamA5 <magamnak5@gmail.com>
2021-03-12 19:43:31 +0100

    HTMLTag kommentelés

--- frontend/utilities/HTMLTag.js
+++ frontend/utilities/HTMLTag.js
@@ -1,31 +1,33 @@
+/**A class for creating HTML structures with flowing setters */
 class HTMLTag{
+    /**tag== htmlTag */
     constructor(tag){
         tag = tag.toString();
         this.node = document.createElement(tag);
         return this;
     };
-    
+    /** Overrides the first argument (attribute) with the second (value)*/
     addAttr(attr, value){
         this.node.setAttribute(attr,value);
         return this;
     }
 
-
+    /**Adds a class to the elements class list */
     addClass(value){
         this.node.classList.add(value);
         return this;
     }
-
+    /**Overrides the text content of the HTMLElement */
     setText(text){
-        this.node.innerHTML = text;
+        this.node.textContent = text;
         return this;
     }
-
+    /** Adds text to the end of the tag*/
     pushText(text){
         this.node.innerHTML += text;
         return this;
     }
-
+    /**Appends this element to the given one */
     append(tag){
         if(tag instanceof HTMLTag){
             tag.node.appendChild(this.node);
@@ -35,11 +37,13 @@ class HTMLTag{
         }
         return this;
     }
-
+    /**Adds the given function to the click event */
     onclick(func){
         this.node.onclick = func;
         return this;
     }
+
+    
 }
 
 export default HTMLTag;
