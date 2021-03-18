d04f2a1b702e4cd4fa2d681db51c0f486fd85721
SAdamA5 <magamnak5@gmail.com>
2021-03-18 16:45:26 +0100

    Az előző commit pár bugjának javítása

--- frontend/view/signup.js
+++ frontend/view/signup.js
@@ -10,7 +10,7 @@ function createSignup(){
     new HTMLTag('input').addAttr('id',formID+'_username').addAttr('name',formID+'_username').addAttr('placeholder','Felhasználónév').addAttr('type','text').append(signupForm);
     new HTMLTag('input').addAttr('id',formID+'_mail').addAttr('name',formID+'_mail').addAttr('placeholder','Email cím').addAttr('type','text').append(signupForm);
     new HTMLTag('input').addAttr('id',formID+'_pass').addAttr('name',formID+'_pass').addAttr('placeholder','Jelszó').addAttr('type','password').append(signupForm);
-    new HTMLTag('input').addAttr('id',formID+'_passAgain').addAttr('name',formID+'_passAgain').addAttr('placeholder','Jelszó').addAttr('type','password').append(signupForm);
+    new HTMLTag('input').addAttr('id',formID+'_passAgain').addAttr('name',formID+'_passAgain').addAttr('placeholder','Jelszó megerősítés').addAttr('type','password').append(signupForm);
     new HTMLTag('a').setText('Bejelentkezés').addAttr('href','').append(signupForm).onclick(new viewController().loadLogin);
     
     new HTMLTag('button').setText('Bejelentkezés').append(signupForm).onclick(new viewController().loadAdmin);
