d225dbcdd4d142fd80fb34c387c46d7c071825dd
Marcell Cseh <csehmarcell@yahoo.com>
2021-03-19 15:55:20 +0100

    datasets

---
+++ frontend/datasets/recordData.js
@@ -0,0 +1,129 @@
+class RecordData{
+    constructor(){
+        this.recordTestData = [
+            {
+                name:'Aranycsapat',
+                projects:[
+                            {
+                                name:'Támadás',
+                                records:[
+                                            {creator:'Puskás',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Puskás',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Tavasz',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Tavasz',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                        ]
+                            },
+                            {
+                                name:'Védekezés',
+                                records:[
+                                            {creator:'Györe',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Györe',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Bambusz',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Bambusz',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                        ]
+                            }
+                        ]
+            },
+            {
+                name:'X-faktor',
+                projects:[
+                            {
+                                name:'Szóló',
+                                records:[
+                                            {creator:'Kata',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Bálint',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Zoltán',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Anita',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                        ]
+                            },
+                            {
+                                name:'Csapat',
+                                records:[
+                                            {creator:'Mercy',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Para',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Tom & Jerry',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Tom & Jerry',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                        ]
+                            },
+                            {
+                                name:'Zsûri',
+                                records:[
+                                            {creator:'Feró',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Feró',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Feró',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Feró',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Feró',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Feró',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                        ]
+                            }
+                        ]
+            },
+            {
+                name:'progi',
+                projects:[
+                            {
+                                name:'frontend',
+                                records:[
+                                            {creator:'Kata',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Bálint',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Zoltán',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Anita',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                        ]
+                            },
+                            {
+                                name:'backend',
+                                records:[
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Zala',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Zala',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Mata',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                        ]
+                            },
+                            {
+                                name:'meeting',
+                                records:[
+                                            {creator:'Kolomp',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Kolomp',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Kolomp',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Feró',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                            {creator:'Grosics',length:'3:15',date:'2021/03/18',desc:"Lorem ipsum dolor sit amet. skadjléaslkdamndkjn jdash fls fdsak ea ldc jdskne lads"},
+                                        ]
+                            }
+                        ]
+            }
+        ]
+    }
+
+    getProjects(){
+        const result = [];
+        for(let team of this.recordTestData){
+            for(let project of team.projects){
+                result.push(project.name);
+            }
+        }
+        return result;
+    }
+
+    getTeams(){
+        const result = [];
+        for(let team of this.recordTestData){
+            result.push(team.name);
+        }
+        return result;
+    }
+}
+
+export default RecordData;
