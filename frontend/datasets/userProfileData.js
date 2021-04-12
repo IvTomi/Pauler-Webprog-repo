b8d54fa3c7c0a08ea0b58e77a48eb299e28217d2
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-12 17:08:08 +0200

    new userProfileData

---
+++ frontend/datasets/userProfileData.js
@@ -0,0 +1,11 @@
+class userProfileData{
+    constructor(){
+        this.testProfileData = [
+            {id:0 ,visible:true, name:'Telefonszám', value:'0630/323233'},
+            {id:1 ,visible:true, name:'E-mail cím', value:'pelda@pelda.hu'},
+            {id:2 ,visible:true, name:'Lakhely', value:'Valahol'},
+        ]
+
+    }
+}
+export default userProfileData;
