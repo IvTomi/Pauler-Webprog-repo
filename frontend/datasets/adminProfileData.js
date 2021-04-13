947203f75df48661ba7464a0ccc3155f3597a87d
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-13 13:00:41 +0200

    testAdminProfileData

---
+++ frontend/datasets/adminProfileData.js
@@ -0,0 +1,11 @@
+class adminProfileData{
+    constructor(){
+        this.testAdminProfileData = [
+            {id:0 ,visible:true, name:'Adószám', value:'04354353234234'},
+            {id:1 ,visible:true, name:'E-mail cím', value:'pelda@pelda.hu'},
+            {id:2 ,visible:true, name:'Telephely', value:'Valahol'},
+        ]
+
+    }
+}
+export default adminProfileData;
