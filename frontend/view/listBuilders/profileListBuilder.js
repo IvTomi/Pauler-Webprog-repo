0fd698f76d24c1ab1177c4ac5287e58d9c441c08
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-13 13:04:36 +0200

    delete option

--- frontend/view/listBuilders/profileListBuilder.js
+++ frontend/view/listBuilders/profileListBuilder.js
@@ -5,6 +5,9 @@ export function createProfileDataList(data,appendPoint){
     const one = new HTMLTag('ul')
     for(let contact of data){
         if(contact.visible){
+            //if admin, or this.user views the profile 
+            new HTMLTag('button').setText('X').append(one);
+            
             new HTMLTag('li').setText(contact.name).append(one);
             new HTMLTag('li').setText(contact.value).append(one);
         }
