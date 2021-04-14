4978b051f88db581b588ca1ec5a58f1acfb8e863
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-14 13:49:59 +0200

    projectTestData létrehozása

---
+++ frontend/datasets/projectData.js
@@ -0,0 +1,44 @@
+class ProjectData{
+    constructor(){
+        this.projectTestData = [
+            {
+                name:'elsoproj',
+                description:'elsoproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                members:
+                [
+                    {name:'Ferenc József',post:'backend'},
+                    {name:'Ferenc Ferdinánd',post:'frontend'},
+                    {name:'Károly Róbert',post:'design'},
+                    {name:'Mária Terézia',post:'leader'},
+                    {name:'Orbán Viktor',post:'management'},
+                ]
+            },
+            {
+                name:'masodikproj',
+                description:'masodikproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                members:
+                [
+                    {name:'Pálinkás József',post:'backend'},
+                    {name:'Ferdinánd Gyula',post:'frontend'},
+                    {name:'Kovács Róbert',post:'design'},
+                    {name:'Szűz Mária',post:'leader'},
+                    {name:'Orbán Sára',post:'management'},
+                ]
+            },
+            {
+                name:'harmadikproj',
+                description:'harmadikproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                members:
+                [
+                    {name:'Jóóóózsef',post:'backend'},
+                    {name:'Gyuszi',post:'frontend'},
+                    {name:'Máté Péter',post:'design'},
+                    {name:'Vona Gábor',post:'leader'},
+                    {name:'Karigeri',post:'management'},
+                ]
+            }
+
+        ]
+    }
+}
+export default ProjectData;
