d52e8c574e9bd01f32fc005df333ff7b2e91bceb
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-18 19:45:00 +0200

    egy projekt

---
+++ frontend/view/content/user/projects/oneProject.js
@@ -0,0 +1,21 @@
+import {refreshContent,setUpListField} from './mainProject.js';
+import HTMLTag from '../../../../utilities/HTMLTag.js';
+import {createProjectMembersList,createProjectRecordList} from '../../../listBuilders/projectListBuilder.js'
+import ProjectData from '../../../../datasets/projectData.js'
+
+
+function createOneProjectView(){
+    const content = refreshContent(2);
+
+    new HTMLTag('h1').setText('Feladatnév').append(content);
+    new HTMLTag('p').setText('Ezt a leírást is amúgy majd adatbázisból kell beolvasni').append(content);
+    new HTMLTag('h3').setText('Tagok').append(content);
+    new HTMLTag('h3').setText('Rekordok').append(content);
+
+    const listDiv = setUpListField(content);
+    createProjectMembersList(new ProjectData().projectTestData,listDiv);
+    createProjectRecordList(new ProjectData().projectTestData,listDiv);
+
+}
+
+export default createOneProjectView;
