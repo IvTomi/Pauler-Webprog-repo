98149f00e3addb992c8079c0621d1ffe46490546
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-18 19:36:38 +0200

    ProjectData bővítése

--- frontend/datasets/projectData.js
+++ frontend/datasets/projectData.js
@@ -4,37 +4,265 @@ class ProjectData{
             {
                 name:'elsoproj',
                 description:'elsoproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:1,
+                end:'2021/04/27',
                 members:
                 [
-                    {name:'Ferenc József',post:'backend'},
-                    {name:'Ferenc Ferdinánd',post:'frontend'},
-                    {name:'Károly Róbert',post:'design'},
-                    {name:'Mária Terézia',post:'leader'},
-                    {name:'Orbán Viktor',post:'management'},
+                    {picture:'./res/defaultUser.png',name:'Ferenc József',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Ferenc Ferdinánd',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Károly Róbert',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Mária Terézia',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Orbán Viktor',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
                 ]
             },
             {
                 name:'masodikproj',
                 description:'masodikproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:0,
+                end:'2021/04/27',
                 members:
                 [
-                    {name:'Pálinkás József',post:'backend'},
-                    {name:'Ferdinánd Gyula',post:'frontend'},
-                    {name:'Kovács Róbert',post:'design'},
-                    {name:'Szűz Mária',post:'leader'},
-                    {name:'Orbán Sára',post:'management'},
+                    {picture:'./res/defaultUser.png',name:'Pálinkás József',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Ferdinánd Gyula',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Kovács Róbert',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Szűz Mária',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Orbán Sára',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
                 ]
             },
             {
                 name:'harmadikproj',
                 description:'harmadikproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:2,
+                end:'2021/04/27',
                 members:
                 [
-                    {name:'Jóóóózsef',post:'backend'},
-                    {name:'Gyuszi',post:'frontend'},
-                    {name:'Máté Péter',post:'design'},
-                    {name:'Vona Gábor',post:'leader'},
-                    {name:'Karigeri',post:'management'},
+                    {picture:'./res/defaultUser.png',name:'Jóóóózsef',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Gyuszi',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Máté Péter',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Vona Gábor',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Karigeri',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                ]
+            },
+            {
+                name:'negyedikproj',
+                description:'elsoproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:1,
+                end:'2021/04/27',
+                members:
+                [
+                    {picture:'./res/defaultUser.png',name:'Ferenc József',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Ferenc Ferdinánd',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Károly Róbert',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Mária Terézia',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Orbán Viktor',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                ]
+            },
+            {
+                name:'otodikproj',
+                description:'masodikproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:0,
+                end:'2021/04/27',
+                members:
+                [
+                    {picture:'./res/defaultUser.png',name:'Pálinkás József',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Ferdinánd Gyula',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Kovács Róbert',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Szűz Mária',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Orbán Sára',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                ]
+            },
+            {
+                name:'hatodikproj',
+                description:'harmadikproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:2,
+                end:'2021/04/27',
+                members:
+                [
+                    {picture:'./res/defaultUser.png',name:'Jóóóózsef',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Gyuszi',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Máté Péter',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Vona Gábor',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Karigeri',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                ]
+            },
+            {
+                name:'hetedikproj',
+                description:'elsoproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:1,
+                end:'2021/04/27',
+                members:
+                [
+                    {picture:'./res/defaultUser.png',name:'Ferenc József',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Ferenc Ferdinánd',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Károly Róbert',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Mária Terézia',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Orbán Viktor',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                ]
+            },
+            {
+                name:'nyolcadikproj',
+                description:'masodikproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:0,
+                end:'2021/04/27',
+                members:
+                [
+                    {picture:'./res/defaultUser.png',name:'Pálinkás József',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Ferdinánd Gyula',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Kovács Róbert',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Szűz Mária',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Orbán Sára',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                ]
+            },
+            {
+                name:'kilencedikproj',
+                description:'harmadikproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:2,
+                end:'2021/04/27',
+                members:
+                [
+                    {picture:'./res/defaultUser.png',name:'Jóóóózsef',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Gyuszi',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Máté Péter',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Vona Gábor',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Karigeri',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                ]
+            },
+            {
+                name:'tizedikproj',
+                description:'elsoproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:1,
+                end:'2021/04/27',
+                members:
+                [
+                    {picture:'./res/defaultUser.png',name:'Ferenc József',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Ferenc Ferdinánd',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Károly Róbert',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Mária Terézia',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Orbán Viktor',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                ]
+            },
+            {
+                name:'tizenegyedikproj',
+                description:'masodikproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:0,
+                end:'2021/04/27',
+                members:
+                [
+                    {picture:'./res/defaultUser.png',name:'Pálinkás József',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Ferdinánd Gyula',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Kovács Róbert',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Szűz Mária',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Orbán Sára',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                ]
+            },
+            {
+                name:'tizenkettedikproj',
+                description:'harmadikproj leiras ada sd d sda sdas d asdd sasads sd sasdad.',
+                status:2,
+                end:'2021/04/27',
+                members:
+                [
+                    {picture:'./res/defaultUser.png',name:'Jóóóózsef',post:'backend'},
+                    {picture:'./res/defaultUser.png',name:'Gyuszi',post:'frontend'},
+                    {picture:'./res/defaultUser.png',name:'Máté Péter',post:'design'},
+                    {picture:'./res/defaultUser.png',name:'Vona Gábor',post:'leader'},
+                    {picture:'./res/defaultUser.png',name:'Karigeri',post:'management'},
+                ],
+                records:
+                [
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc Ferdinánd',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Ferenc József',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
+                    {author:'Mária Terézia',dateTime:'2023/01/23 10:10',message:"asd asdas dasd as das das asddds addas d d asdasdasd"},
                 ]
             }
 
