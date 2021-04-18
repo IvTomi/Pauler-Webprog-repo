ceb09b319911547b689c1800bffc253c1b783dfe
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-18 19:40:14 +0200

    admin project választó

---
+++ frontend/view/content/admin/project/mainProject.js
@@ -0,0 +1,42 @@
+import changeHighlithed from '../../../../utilities/changeHighlighted.js';
+import HTMLTag from '../../../../utilities/HTMLTag.js';
+import viewController from '../../../../controllers/viewController.js';
+import makeNewProjectView from '../project/newProject.js';
+import createAllProjectsView from './allProjects.js';
+
+
+function setUpAdminProjectsView(appendPoint){
+
+    const navList = document.querySelector('nav ul');
+    changeHighlithed(1,navList);
+
+    const selecter = new HTMLTag('ul').addAttr('id','selecter').append(appendPoint);
+    new HTMLTag('div').addAttr('id','content').append(appendPoint);
+
+    new HTMLTag('li').setText('Meglévő projektek').append(selecter).onclick(makeNewProjectView);
+    new HTMLTag('li').setText('projekt létrehozása').append(selecter).onclick(createAllProjectsView);
+    
+}
+export function refreshContent(n){
+    const selecters = document.getElementById('selecter');
+    changeHighlithed(n,selecters);
+    const content = document.getElementById('content');
+    new viewController().clearTag(content);
+    return content;
+}
+
+export function setUpListField(appendPoint){
+    let list = document.getElementById('list');
+        if(list){
+            new viewController().clearTag(list);
+            return list;
+        }
+        else{
+            list = document.createElement('ul');
+            list.setAttribute('id','list');
+            appendPoint.appendChild(list);
+            return list;
+        }
+}
+
+export default setUpAdminProjectsView;
