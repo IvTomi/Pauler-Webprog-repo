5d8d1d6466e44d76d3942f80717f3465a4fe0886
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-18 19:44:22 +0200

    projektjeim

--- frontend/view/content/user/projects/myProjects.js
+++ frontend/view/content/user/projects/myProjects.js
@@ -1,21 +1,25 @@
 import {refreshContent,setUpListField} from './mainProject.js';
 import HTMLTag from '../../../../utilities/HTMLTag.js';
-import createMyProjectList from '../../../listBuilders/projectListBuilder.js'
+import {createMyProjectList} from '../../../listBuilders/projectListBuilder.js'
 import ProjectData from '../../../../datasets/projectData.js'
 
 function createMyProjectsView(){
     const content = refreshContent(0);
 
-    let select = new HTMLTag('select').setText("asd").append(content);
-    new HTMLTag('option').addAttr('value',0).setText('Érték választása').append(select);
-    new HTMLTag('option').addAttr('value',1).setText('befejezett').append(select);
-    new HTMLTag('option').addAttr('value',2).setText('folyamatban').append(select);
-    new HTMLTag('option').addAttr('value',3).setText('mindennapos').append(select);
+    let mySelect = new HTMLTag('select').addAttr('id','mySelect').append(content);
+    new HTMLTag('option').addAttr('value',0).setText('Érték választása').append(mySelect);
+    new HTMLTag('option').addAttr('value',1).setText('befejezett').append(mySelect);
+    new HTMLTag('option').addAttr('value',2).setText('folyamatban').append(mySelect);
+    new HTMLTag('option').addAttr('value',3).setText('mindennapos').append(mySelect);
 
+    mySelect.addEventListener('change', myProjectUpdate);
+
+    
+}
+
+function myProjectUpdate(){
     const listDiv = setUpListField(content);
-    createMyProjectList(new ProjectData().projectTestData,listDiv);
-
-
+    createMyProjectList(new ProjectData().projectTestData,listDiv,mySelect.value);
 }
 
 export default createMyProjectsView;
