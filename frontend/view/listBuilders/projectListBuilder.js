b96f6a575d5287dffe6fe30af3ae4004ba02579e
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-20 18:58:05 +0200

    új projektlistbuilderek

--- frontend/view/listBuilders/projectListBuilder.js
+++ frontend/view/listBuilders/projectListBuilder.js
@@ -1,8 +1,6 @@
 import HTMLTag from "../../utilities/HTMLTag.js";
 
 export function createMyProjectList(data,appendPoint,option){
-    
-
     const ulList = new HTMLTag('div');
     for(let project of data){
         const one = new HTMLTag('ul');
@@ -16,7 +14,7 @@ export function createMyProjectList(data,appendPoint,option){
 export function createProjectMembersList(data,appendPoint){
     let div = new HTMLTag('div');
     let one = new HTMLTag('ul').append(div);
-    for(let member of data[0].members){
+    for(let member of data.members){
         new HTMLTag('img').addAttr('src',member.picture).addAttr('alt','user képe').append(one);
         new HTMLTag('li').setText(member.name).append(one);
         new HTMLTag('li').setText(member.post).append(one);
@@ -29,7 +27,7 @@ export function createProjectRecordList(data,appendPoint)
 {
     let div = new HTMLTag('div');
     let one = new HTMLTag('ul').append(div);
-    for(let record of data[0].records){
+    for(let record of data.records){
         new HTMLTag('li').setText(record.author).append(one);
         new HTMLTag('li').setText(record.dateTime).append(one);
         new HTMLTag('li').setText(record.message).append(one);
@@ -55,3 +53,46 @@ export function createAllProjectList(data,appendPoint){
     ulList.append(appendPoint);
 }
 
+export function CreateOneProjectMemberList(data, appendPoint){
+    let div = new HTMLTag('div');
+    let allMembers = new HTMLTag('div');
+    new HTMLTag('p').setText('Hozzáadottak').append(div);
+    let one = new HTMLTag('ul');
+    for(let member of data){
+        new HTMLTag('img').addAttr('src',member.picture).addAttr('alt','user képe').append(one);
+        new HTMLTag('p').setText(member.name).append(one);
+        new HTMLTag('button').setText("törlés").addClass('btn_del').append(one).onclick(()=>console.log('Clicked: '+ member.name));
+        one.append(allMembers);
+    }
+    allMembers.append(div).addClass('scroll');
+    div.append(appendPoint);
+}
+
+export function CreateOneProjectNonMemberList(data, appendPoint){
+    let div = new HTMLTag('div');
+    let allMembers = new HTMLTag('div');
+    new HTMLTag('p').setText('Alkalmazottak').append(div);
+    let one = new HTMLTag('ul');
+    for(let member of data){
+        new HTMLTag('img').addAttr('src',member.picture).addAttr('alt','user képe').append(one);
+        new HTMLTag('p').setText(member.name).append(one);
+        new HTMLTag('button').setText("Hozzáadás").addClass('btn_add').append(one).onclick(()=>console.log('Add: '+ member.id));
+        one.append(allMembers);
+    }
+    allMembers.append(div).addClass('scroll');
+    div.append(appendPoint);
+}
+
+export function CreateNewProjectNonTeamList(data,appendPoint){
+    const div = new HTMLTag('div');
+    const allTeams = new HTMLTag('div');
+    new HTMLTag('p').setText('Csapatok').append(div);
+    const one = new HTMLTag('ul');
+    for(let team of data){
+        new HTMLTag('p').setText(team.name).append(one);
+        new HTMLTag('button').setText("Hozzáadás").addClass('btn_add').append(one).onclick(()=>console.log('Add: '+ team.id));
+        one.append(allTeams);
+    }
+    allTeams.append(div).addClass('scroll');
+    div.append(appendPoint);
+}
