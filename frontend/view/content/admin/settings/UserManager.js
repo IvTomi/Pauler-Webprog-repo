49025058ccb59e247b214d5d1b0d033b32295603
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-28 14:23:30 +0200

    Admin settings átalakításának elkezdése

--- frontend/view/content/admin/settings/UserManager.js
+++ frontend/view/content/admin/settings/UserManager.js
@@ -3,10 +3,10 @@ import {refreshContent} from './mainSettings.js';
 
 function createUserManagerView(){
     const content = refreshContent(1);
-
+    new HTMLTag('p').setText('User manager works').append(content);
     //ez a rész majd egy listbuilderbe fog kerülni(és annyiszor ismétlődni, ahány alk. van)
     //nem kéne egy kereső?
-    const formID= 'changeForm';
+    /*const formID= 'changeForm';
     const changeForm = new HTMLTag('form').addAttr('id',formID).append(content);
 
     new HTMLTag('p').setText('Régi jelszó').append(changeForm);
@@ -15,7 +15,8 @@ function createUserManagerView(){
     new HTMLTag('input').addAttr('id',formID+'_pass').addAttr('name',formID+'_pass').addAttr('type','password').append(changeForm);
     new HTMLTag('p').setText('Jelszó megerősítése').append(changeForm);
     new HTMLTag('input').addAttr('id',formID+'_passAgain').addAttr('name',formID+'_passAgain').addAttr('type','password').append(changeForm);
-    
+    */
+   
     //jogosultságok felsorolása
 }
 
--- frontend/view/content/admin/settings/mainSettings.js
+++ frontend/view/content/admin/settings/mainSettings.js
@@ -1,19 +1,27 @@
 import changeHighlithed from '../../../../utilities/changeHighlighted.js';
 import HTMLTag from '../../../../utilities/HTMLTag.js';
 import createNewUserView from './CreateUser.js'
-import CreateUserManagerVieW from './UserManager.js'
+import CreateUserManagerView from './UserManager.js'
 import viewController from '../../../../controllers/viewController.js';
+import {router} from '../../../../index.js';
 
-function setUpAdminSettingsView(appendPoint){
+function setUpAdminSettingsView(appendPoint,n){
     const navList = document.querySelector('nav ul');
-    changeHighlithed(3,navList);
+    changeHighlithed(1,navList);
 
     const selecter = new HTMLTag('ul').addAttr('id','selecter').append(appendPoint);
     new HTMLTag('div').addAttr('id','content').append(appendPoint);
 
-    new HTMLTag('li').setText('Fiók létrehozása').append(selecter).onclick(createNewUserView);
-    new HTMLTag('li').setText('Felhasználók kezelése').append(selecter).onclick(CreateUserManagerVieW);
+    new HTMLTag('li').setText('Felhasználók kezelése').append(selecter).onclick(()=>{router.navigate('manageSettingsAdmin')});
+    new HTMLTag('li').setText('Fiók létrehozása').append(selecter).onclick(()=>{router.navigate('newSettingsAdmin')});
 
+    if(n === 0){
+        CreateUserManagerView();
+    }
+    else if(n === 1){
+        createNewUserView();
+    }
+     
 }
 export function refreshContent(n){
     const selecters = document.getElementById('selecter');
