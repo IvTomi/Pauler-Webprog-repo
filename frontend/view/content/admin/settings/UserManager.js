2db496f525f836937bdc7da7c6d1235ed8a31610
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-29 12:13:59 +0200

    usermanager átalakítás

--- frontend/view/content/admin/settings/UserManager.js
+++ frontend/view/content/admin/settings/UserManager.js
@@ -1,23 +1,13 @@
 import HTMLTag from '../../../../utilities/HTMLTag.js';
 import {refreshContent} from './mainSettings.js';
+import {ManageUser} from '../../../../controllers/settingsController.js'
 
 function createUserManagerView(){
     const content = refreshContent(1);
-    new HTMLTag('p').setText('User manager works').append(content);
-    //ez a rész majd egy listbuilderbe fog kerülni(és annyiszor ismétlődni, ahány alk. van)
-    //nem kéne egy kereső?
-    /*const formID= 'changeForm';
-    const changeForm = new HTMLTag('form').addAttr('id',formID).append(content);
 
-    new HTMLTag('p').setText('Régi jelszó').append(changeForm);
-    new HTMLTag('input').addAttr('id',formID+'_oldpass').addAttr('name',formID+'_oldpass').addAttr('type','password').append(changeForm);
-    new HTMLTag('p').setText('Új jelszó').append(changeForm);
-    new HTMLTag('input').addAttr('id',formID+'_pass').addAttr('name',formID+'_pass').addAttr('type','password').append(changeForm);
-    new HTMLTag('p').setText('Jelszó megerősítése').append(changeForm);
-    new HTMLTag('input').addAttr('id',formID+'_passAgain').addAttr('name',formID+'_passAgain').addAttr('type','password').append(changeForm);
-    */
-   
-    //jogosultságok felsorolása
+    //if(van rá jogosultsága)
+    ManageUser(content);
+    
 }
 
 export default createUserManagerView;
