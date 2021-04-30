521db167dbb6fa9a1f80e28b80de0f28c29a4e1a
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-04-30 11:41:01 +0200

    Új userProfileController

--- frontend/controllers/userProfileController.js
+++ frontend/controllers/userProfileController.js
@@ -1,4 +1,81 @@
 import HTMLTag from '../utilities/HTMLTag.js';
 import {makeRequest} from '../utilities/serviceHandler.js';
 import {router} from '../index.js';
+import { getHeader } from './logincontroller.js';
 
+
+export function allUsers(){
+    makeRequest('/user/list','POST',getHeader(),'{}',(data)=>{OnListSuccess(data)},()=>{OnListFail()});
+
+}
+
+function OnListSuccess(data){
+    return data;
+    //a kapott userlistet hogy adom vissza a listbuildernek?
+}
+
+function OnListFail(){
+    console.log('hibbaaaa');
+}
+
+
+export function addContact(){
+
+    makeRequest('/user/add/contact','POST',getHeader(),JSON.stringify({/*"userid":user.id,"contactid":contact.id*/}),(data)=>{OnAddSuccess(data)},()=>{OnAddFail()});
+
+}
+function OnAddSuccess(){
+    
+}
+
+function OnAddFail(){
+    
+}
+
+
+export function removeContact(userid,contactid){
+
+    makeRequest('/user/remove/contact','POST',getHeader(),JSON.stringify({"userid":userid,"contactid":contactid}),(data)=>{OnRemoveSuccess(data)},()=>{OnRemoveFail()});
+
+}
+
+function OnRemoveSuccess(data){console.log("sucessdelete" + data);}
+
+function OnRemoveFail(){console.log("faildelete");}
+
+
+export function getContacts(userid){
+
+    makeRequest('/user/get/contacts','POST',getHeader(),JSON.stringify({"userid":userid}),(data)=>{getContactsSuccess(data)},()=>{getContactsFail()});
+
+}
+
+function getContactsSuccess(){}
+
+function getContactsFail(){}
+
+export function CreateContact(userid){
+    let typeRead = document.getElementById('type').value;
+    let valueRead = document.getElementById('value').value;
+    if(typeRead && valueRead){
+        makeRequest('/contact/create','POST',getHeader(),JSON.stringify({"typename":typeRead,"value":valueRead,"description":"","ispublic":"true"}),(data)=>{CreateContactSuccess(data,userid)},()=>{CreateContactFail()});
+    }
+}
+
+function CreateContactSuccess(data,userid){
+    
+    makeRequest('/user/add/contact','POST',getHeader(),JSON.stringify({"userid":userid,"contactid":data.contactid}),(data)=>{addContactSuccess(data)},()=>{addContactFail()});
+
+}
+
+function CreateContactFail(){
+    alert("Fail ág");
+}
+
+function addContactSuccess(data){
+    //feltétélek
+}
+
+function addContactFail(){
+    alert("Fail ág");
+}
