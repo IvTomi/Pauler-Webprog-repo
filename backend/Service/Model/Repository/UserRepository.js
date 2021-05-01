5845fd36181d2e48386bb8e451ce3b5334fcd5f1
rumplerbalint <rumplerbalint@gmail.com>
2021-05-01 11:47:14 +0200

    userek

--- backend/Service/Model/Repository/UserRepository.js
+++ backend/Service/Model/Repository/UserRepository.js
@@ -27,8 +27,9 @@ async function authenticateUser(username,hash,password){
     return new Promise((resolve,reject)=>{
         getAuthenticateUser(username,hash).then((result)=>{
             if(result === null){
-                resolve(JSON.stringify(jsonParser.combineJSON(protocol.status(false),protocol.error(1))));
+                resolve(jsonParser.combineJSON(protocol.status(false),protocol.error(1)));
                 logger.debug('query result NULL');
+                logger.debug(result);
             }else{
                 try{
                     if(password === encryptor.decrypt(result[0][0]["Cypher"])){
@@ -140,7 +141,7 @@ async function userByHash(userid, hash)
     })
 }
 
-//GET USERS
+//GET USERS (nem userobject-et ad vissza)
 async function getUsers(hash)
 {
     return new Promise((resolve, reject)=>
@@ -149,7 +150,7 @@ async function getUsers(hash)
         {
             res.awaitQuery(`CALL GetUsers('${hash}')`).then((result)=>
             {
-                resolve(result);
+                resolve(result[result.length-2]);
             }).catch((e)=>
             {
                 resolve(null);
@@ -170,8 +171,9 @@ async function users(hash)
             }
             else
             {
-                logger.debug(JSON.stringify(result[0]));
-                resolve(result[0]);
+                logger.debug(JSON.stringify(result));
+                resolve((jsonParser.combineJSON(protocol.status(true),ViewToUser(result))));
+                //resolve(result);
             }
         })
     })
@@ -251,9 +253,89 @@ async function userPermissions(userid)
     })
 }
 
-//MODIFY USER
+//MODIFY USER (az eredmény nem a user obj még) [probléma sql-ben CSAK jelszóváltoztatás VAGY CSAK first/last name változtatás]
+async function modifyUser(targetuserid, password, firstname, lastname, idmodifiedBy)
+{
+    return new Promise((resolve, reject)=>
+    {
+        pool.awaitGetConnection().then((res)=>
+        {
+            res.awaitQuery(`CALL ModifyUser(${targetuserid},'${password}','${firstname}','${lastname}',${idmodifiedBy})`).then((result)=>
+            {
+                resolve(result);
+            }).catch((e)=>
+            {
+                resolve(null);
+            })
+            res.release();
+        })
+    })
+}
+async function changeUser(targetuserid, password, firstname, lastname, idmodifiedBy)
+{
+    return new Promise((resolve, reject)=>
+    {
+        if(!targetuserid)
+        {
+            resolve(jsonParser.combineJSON(protocol.status(false),protocol.error(3)));
+        }
+        modifyUser(targetuserid, password, firstname, lastname, idmodifiedBy).then((result)=>
+        {
+            if(result==null)
+            {
+                resolve(jsonParser.combineJSON(protocol.status(false),protocol.error(1)));
+            }
+            else
+            {
+                logger.debug(JSON.stringify(result));
+                resolve(jsonParser.combineJSON(protocol.status(true),result));
+            }
+        })
+    })
+}
 
-//CREATE USER
+//REMOVE USER ("user not exist" hibát nem ad vissza)
+async function deleteUser(userid, idlastmodified)
+{
+    return new Promise((resolve, reject)=>
+    {
+        pool.awaitGetConnection().then((res)=>
+        {
+            res.awaitQuery(`CALL DeleteUser(${userid},${idlastmodified})`).then((result)=>
+            {
+                resolve(result);
+            }).catch((e)=>
+            {
+                resolve(null);
+            })
+            res.release();
+        })
+    })
+}
+async function removeUser(userid, idlastmodified)
+{
+    return new Promise((resolve, reject)=>
+    {
+        if(!userid || !idlastmodified)
+        {
+            resolve(jsonParser.combineJSON(protocol.status(false),protocol.error(3)));
+        }
+        deleteUser(userid, idlastmodified).then((result)=>
+        {
+            if(result==null)
+            {
+                resolve(jsonParser.combineJSON(protocol.status(false),protocol.error(1)));
+            }
+            else
+            {
+                logger.debug(JSON.stringify(result));
+                resolve(jsonParser.combineJSON(protocol.status(true)));
+            }
+        })
+    })
+}
+
+//CREATE USER (nem user objectet ad vissza)
 async function createUser(username, password, firstname, lastname, hash, userid, allprivileges)
 {
     return new Promise((resolve, reject)=>
@@ -275,7 +357,7 @@ async function userCreator(username, password, firstname, lastname, hash, userid
 {
     return new Promise((resolve, reject)=>
     {
-        if(!username || !password || !hash || !userid || !allprivileges)
+        if(!username || !password || !hash || !userid)
         {
             resolve(jsonParser.combineJSON(protocol.status(false),protocol.error(3)));
         }
@@ -288,16 +370,138 @@ async function userCreator(username, password, firstname, lastname, hash, userid
         {
             if(result==null)
             {
-                resolve(jsonParser.combineJSON(protocol.status(false),protocol.error(4)));
+                resolve(jsonParser.combineJSON(protocol.status(false),protocol.error(1)));
             }
             else
             {
                 logger.debug(JSON.stringify(result));
-                resolve(true);
+                resolve(protocol.status(true));
             }
         })
     })
 }
+
+//CREATE CONTACT (Csak id-t ad vissza, nem contact obj-ot)
+async function createContact(typename, value, description, userid, ispublic, hash)
+{
+    return new Promise((resolve, reject)=>
+    {
+        pool.awaitGetConnection().then((res)=>
+        {
+            res.awaitQuery(`CALL CreateContact('${typename}','${value}','${description}',${userid},${ispublic},'${hash}')`).then((result)=>
+            {
+                resolve(result);
+            }).catch((e)=>
+            {
+                resolve(null);
+            })
+            res.release();
+        })
+    })
+}
+async function newContact(typename, value, description, userid, ispublic, hash)
+{
+    return new Promise((resolve, reject)=>
+    {
+        if(!description) description="";
+        createContact(typename, value, description, userid, ispublic, hash).then((result)=>
+        {
+            if(result==null)
+            {
+                resolve(jsonParser.combineJSON(protocol.status(false),protocol.error(1)));
+            }
+            else
+            {
+                logger.debug(JSON.stringify(result));
+                resolve(jsonParser.combineJSON(protocol.status(true),result[0][0]));
+            }
+        })
+    })
+}
+
+//MODIFY CONTACT (Csak id-t, nem contact obj-ot ad még vissza) (contact not exist eset nincs megírva)
+async function modifyContact(contactId, value, description, ispublic, userid)
+{
+    return new Promise((resolve, reject)=>
+    {
+        pool.awaitGetConnection().then((res)=>
+        {
+            res.awaitQuery(`CALL ModifyContact(${contactId},'${value}','${description}',${ispublic},${userid})`).then((result)=>
+            {
+                resolve(result);
+            }).catch((e)=>
+            {
+                resolve(null);
+            })
+            res.release();
+        })
+    })
+}
+async function changeContact(contactId, value, description, ispublic, userid)
+{
+    return new Promise((resolve, reject)=>
+    {
+        if(!description || !value || !ispublic)
+        {
+            description="";
+            value="";
+            ispublic=true;
+        }
+        modifyContact(contactId, value, description, ispublic, userid).then((result)=>
+        {
+            if(result==null)
+            {
+                resolve(jsonParser.combineJSON(protocol.status(false),protocol.error(1)));
+            }
+            else
+            {
+                //ide egy if a contact not existre
+                logger.debug(JSON.stringify(result));
+                resolve(jsonParser.combineJSON(protocol.status(true),result));
+            }
+        })
+    })
+}
+
+//DELETE CONTACT (a "contactId nem létezik" hiba nincs megírva)
+async function deleteContact(contactId, userid)
+{
+    return new Promise((resolve, reject)=>
+    {
+        pool.awaitGetConnection().then((res)=>
+        {
+            res.awaitQuery(`CALL DeleteContact(${contactId},${userid})`).then((result)=>
+            {
+                resolve(result);
+            }).catch((e)=>
+            {
+                resolve(null);
+            })
+            res.release();
+        })
+    })
+}
+async function contactDeleter(contactId, userid)
+{
+    return new Promise((resolve, reject)=>
+    {
+        deleteContact(contactId,userid).then((result)=>
+        {
+            if(result==null)
+            {
+                resolve(jsonParser.combineJSON(protocol.status(false),protocol.error(1)));
+            }
+            else
+            {
+                //ide egy if a contact not existre
+                logger.debug(JSON.stringify(result));
+                resolve(jsonParser.combineJSON(protocol.status(true)));
+            }
+        })
+    })
+}
+
+
 
 module.exports={
     authenticateUser:authenticateUser,
@@ -307,5 +511,9 @@ module.exports={
     userContacts:userContacts,
     userPermissions:userPermissions,
     userCreator:userCreator,
-
+    newContact:newContact,
+    changeContact:changeContact,
+    contactDeleter:contactDeleter,
+    removeUser:removeUser,
+    changeUser:changeUser,
 }
