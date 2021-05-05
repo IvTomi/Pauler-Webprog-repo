21c13466aba263448e85a9ccdd001972603bff1a
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-05-05 23:28:20 +0200

    teams

--- frontend/view/content/user/teams/oneTeamU.js
+++ frontend/view/content/user/teams/oneTeamU.js
@@ -4,19 +4,20 @@ import viewController from '../../../../controllers/viewController.js';
 
 function createUsersTeamInfoView(name,desc,members,projects){
     //team = new teamData().teamTestData[1];//Egyelőre fixen meg van adva melyik csapatot töltik be
-    if(!(team.name && team.desc && team.members && team.projects)){//A szükséges információk meglétének ellenőrzése
+    if(!(name && desc /*&& members && projects*/)){//A szükséges információk meglétének ellenőrzése
         return false;
     }
+
     const content = document.getElementById('content');
     new viewController().clearTag(content);
-    new HTMLTag('h2').setText(team.name).append(content);
-    new HTMLTag('p').setText(team.desc).append(content);
+    new HTMLTag('h2').setText(name).append(content);
+    new HTMLTag('p').setText(desc).append(content);
     new HTMLTag('p').setText('Tagok').append(content);
     const memList = new HTMLTag('ul').addClass('members').append(content);
-    createTeamMembersList(team.members,memList,{});
+    createTeamMembersList(members.Users,memList,{});
     new HTMLTag('p').setText('Projektek').append(content);
     const projList = new HTMLTag('ul').addClass('projects').append(content); 
-    createTeamProjectsList(team.projects,projList,{});
+    createTeamProjectsList(projects.Tasks,projList,{});
 }
 
 export default createUsersTeamInfoView;
