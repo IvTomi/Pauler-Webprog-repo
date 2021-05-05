99bcb6ba1d683999f54c1c2e38586236c5eccb23
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-05-05 23:27:11 +0200

    teams

--- frontend/controllers/userOneTeamController.js
+++ frontend/controllers/userOneTeamController.js
@@ -5,24 +5,16 @@ import { getHeader } from '../utilities/sessionJanitor.js';
 import createUsersTeamInfoView from '../view/content/user/teams/oneTeamU.js';
 
 export function getTeamAttributes(id,name,desc){
-
-    makeRequest('/team/get/tasks','POST',getHeader(),JSON.stringify({"Teamid":id}),(data)=>{onGetTasksSucces(data,name,desc)},()=>onAjaxFail());
-
+    makeRequest('/team/get/tasks','POST',getHeader(),JSON.stringify({"Teamid":id}),(data)=>{onGetTasksSuccess(data,id,name,desc)},()=>onAjaxFail());
 }
 
-
-
-function onGetTasksSucces(data,name,desc,id){
-
-    makeRequest('/team/get/users','POST',getHeader(),JSON.stringify({"Teamid":id}),(data2)=>{onGetUsersSucces(data,data2,name,desc,id)},()=>onAjaxFail());
+function onGetTasksSuccess(data,id,name,desc){
+    makeRequest('/team/get/users','POST',getHeader(),JSON.stringify({"Teamid":id}),(data2)=>{onGetUsersSucces(data2,data,name,desc)},()=>onAjaxFail());
 }
 
-function onGetUsersSucces(data,data2,name,desc){
-
-    createUsersTeamInfoView(name,desc,data,data2);
-    
+function onGetUsersSucces(data2,data,name,desc){
+    createUsersTeamInfoView(name,desc,data2,data);
 }
-
 
 function onAjaxFail(){
     console.log("onAjaxFail");
