46c676f9c98af227faf13846c23654e967eaf6f3
IvTomi <terraled54@gmail.com>
2021-05-06 13:16:11 +0200

    fix

--- backend/DataBase/StoredProcedures/GetTaskRecords.sql
+++ backend/DataBase/StoredProcedures/GetTaskRecords.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 SELECT "Creating procedure GetTaskRecords" $$
 DROP PROCEDURE IF EXISTS GetTaskRecords $$
-CREATE PROCEDURE `GetTaskRecords`(IN taskid INT, in recordid INT)
+CREATE PROCEDURE `GetTaskRecords`(IN taskid INT)
 BEGIN
 SELECT * FROM recordview r WHERE Task_idTask=taskid;
 END;
