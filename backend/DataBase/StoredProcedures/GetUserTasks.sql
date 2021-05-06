f864d59446ae975b3a0c3c143ac029eae00d7ca3
IvTomi <terraled54@gmail.com>
2021-05-06 11:10:30 +0200

    sp fix

--- backend/DataBase/StoredProcedures/GetUserTasks.sql
+++ backend/DataBase/StoredProcedures/GetUserTasks.sql
@@ -3,9 +3,9 @@ SELECT "Creating procedure GetUserTasks" $$
 DROP PROCEDURE IF EXISTS GetUserTasks $$
 CREATE PROCEDURE `GetUserTasks`(IN userid INT)
 BEGIN
-SELECT t.Task_idTask,t.TaskName,t.Description,t.StatusName, t.User_idUser FROM usertasksview u RIGHT JOIN
+SELECT DISTINCT t.Task_idTask,t.TaskName,t.Description,t.StatusName, t.User_idUser FROM usertasksview u CROSS JOIN
 (SELECT tuw.User_idUser, ttw.Task_idTask ,ttw.TaskName,ttw.Description,ttw.StatusName FROM teamtasksview ttw INNER JOIN teamusersview tuw 
-ON ttw.Team_idTeam = tuw.Team_idTeam) AS t ON t.Task_idTask = u.Task_idTask WHERE t.User_idUser=userid;
+ON ttw.Team_idTeam = tuw.Team_idTeam) AS t WHERE t.User_idUser=userid;
 END
 $$
 
