223b6c6034a4443a70beaffad990f0050d4cc922
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-05-06 03:08:26 +0200

    projects

--- frontend/view/content/user/projects/myProjects.js
+++ frontend/view/content/user/projects/myProjects.js
@@ -1,12 +1,13 @@
 import {refreshContent,setUpListField} from './mainProject.js';
 import {createMyProjectList} from '../../../listBuilders/projectListBuilder.js'
 import ProjectData from '../../../../datasets/projectData.js'
+import { router } from '../../../../index.js';
 
-function createMyProjectsView(){
+function createMyProjectsView(data){
+    console.log(data);
     const content = refreshContent(0);
-
     const listDiv = setUpListField(content);
-    createMyProjectList(new ProjectData().projectTestData,listDiv);
+    createMyProjectList(data,listDiv);
     
     
 }
