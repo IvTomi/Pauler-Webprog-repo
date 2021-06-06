bb2cbe405c3487159a833725631b9995ab86a1ce
Hollósi Valentin <hollosi.valentin3@gmail.com>
2021-06-06 17:29:19 +0200

    kattintható feladat

--- frontend/controllers/userOneProjectController.js
+++ frontend/controllers/userOneProjectController.js
@@ -2,26 +2,26 @@ import HTMLTag from '../utilities/HTMLTag.js';
 import {makeRequest} from '../utilities/serviceHandler.js';
 import {router} from '../index.js';
 import { getHeader } from '../utilities/sessionJanitor.js';
-import {createOneProjectView} from '../view/content/user/projects/oneProject.js';
+import {createProjectMembersList, createProjectRecordList} from '../view/listBuilders/projectListBuilder.js';
 
-export function getTaskAttributes(id,name,desc){
+
+export function getTaskAttributes(id,listDiv){
     console.log(id);
-    console.log(name);
-    console.log(desc);
-    makeRequest('/task/get/users','POST',getHeader(),JSON.stringify({"Taskid":id}),(data)=>{onGetUsersSuccess(data,id,name,desc)},()=>onAjaxFail());
+
+    makeRequest('/task/get/users','POST',getHeader(),JSON.stringify({"Taskid":id}),(data)=>{onGetUsersSuccess(data,listDiv)},()=>onAjaxFail());
+}
+function onGetUsersSuccess(data,listDiv){
+    createProjectMembersList(data,listDiv)
+};
+
+export function getTaskRecords(id,listDiv){
+    
+    makeRequest('/task/get/records','POST',getHeader(),JSON.stringify({"Taskid":id}),(data)=>{onGetRecordsSuccess(data,listDiv)},()=>onAjaxFail());
 }
 
-function onGetUsersSuccess(data,id,name,desc){
-    console.log(data);
-
-    makeRequest('/task/get/records','POST',getHeader(),JSON.stringify({"Taskid":id}),(data2)=>{onGetUsersSucces(data2,data,name,desc)},()=>onAjaxFail());
-}
-
-function onGetUsersSucces(data2,data,name,desc){
-    console.log(data2); //records
-    console.log(data); //users
+function onGetRecordsSuccess(data,listDiv){
     router.navigate("oneProjectUser");
-    createOneProjectView(name,desc,data2.Records,data.Users);
+    createProjectRecordList(data,listDiv);
 }
 
 function onAjaxFail(){
